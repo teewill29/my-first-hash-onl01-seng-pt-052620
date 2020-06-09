e628874f084a8702522459a9dac0e04ec32810b1
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 23:14:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+latest_hash = {"value" => "key", "value2" => "key2"}
   
 end
 
